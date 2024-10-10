d482ec156dcb8f5d0906e7d8fcd76fab7d4507ce
CREDETECH03\CRDE-TECH <gurkan.erginer@crede-tech.com>
2024-10-10 13:32:17 +0300

    login page created

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { useRouter } from "next/navigation"; // next/navigation'dan useRouter'ı import edin
 
+
 interface NavbarProps {
   userName: string;
   userSurname: string;
@@ -15,21 +16,35 @@ const Navbar: React.FC<NavbarProps> = ({ userName, userSurname, userRole }) => {
     router.push("/rol-yetki"); // Yeni sayfaya yönlendirme
   };
 
+  const handleLogout =  () => {
+    
+    router.push("/login"); // Giriş sayfasına yönlendirme
+  };
+
   return (
     <nav className="bg-gradient-to-bl from-blue-900 to-green-500 p-5 h-full flex items-center justify-between relative">
       <h1 className="text-white text-2xl font-bold flex-grow text-center">
         CREDENCE YETKİLENDİRME PANELİ
-      </h1>
-      <div className="text-white text-lg">
+        <div className="text-white text-lg">
         {userName} {userSurname} ({userRole})
       </div>
 
+      </h1>
+      
       {/* Sol alt köşeye buton ekleniyor */}
       <button
         onClick={handleButtonClick}
         className="absolute bottom-4 left-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       >
         Rol-Yetki Güncelleme
+      </button>
+
+      {/* Çıkış yap butonu ekleniyor */}
+      <button
+        onClick={handleLogout}
+        className="absolute bottom-4 right-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Çıkış Yap
       </button>
     </nav>
   );
