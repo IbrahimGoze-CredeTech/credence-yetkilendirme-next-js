afbb4774d23e4b4f6e321b0daf9c404b2619f0c1
BurakPozut-CredeTech <burak.pozut@crede-tech.com>
2024-11-11 18:05:32 +0300

    Gunluk AreaChart  Added

--- app/rapor/page.tsx
+++ app/rapor/page.tsx
@@ -1,32 +1,44 @@
 'use client'
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
-import { fetcherGet } from '@/utils';
+import { DataItem, fetcherGet, formatDataList } from '@/utils';
 import { useSession } from 'next-auth/react';
 import React, { useEffect, useState, useTransition } from 'react'
 import TalepYaratmaGrid from './_components/talep-yaratma-grid';
 import ImzaAtmaGrid from './_components/imza-atma-grid';
 import ImzaAtananGrid from './_components/imza-atanan-grid';
+import { AreaChartComp } from './_components/area-chart';
+
 
 export default function RaporPage() {
   const session = useSession();
+
   const [yaratma, setYaratma] = useState<[]>([]);
   const [imzaAtma, setImzaAtma] = useState<[]>([]);
   const [imzaAtanan, setImzaAtanan] = useState<[]>([]);
+  const [gunlukTalepYaratma, setGunlukTalepYaratma] = useState<DataItem[]>([]);
+  const [gunlukImzaAtma, setGunlukImzaAtma] = useState<DataItem[]>([]);
 
   const [isPending, startTransition] = useTransition();
 
   useEffect(() => {
     startTransition(async () => {
-      const [rolAtamalar, imzaAtma, imzaAtanan] = await Promise.all([
+      const [rolAtamalar, imzaAtma, imzaAtanan, gunlukTalepYaratma, gunlukImzaAtma] = await Promise.all([
         fetcherGet("/Matris/talep-yaratma-matris", session.data?.token),
         fetcherGet("/Matris/imza-atma-matris", session.data?.token),
         fetcherGet("/Matris/imza-atanan-matris", session.data?.token),
+        fetcherGet("/Matris/talep-yaratma-gunluk-matris", session.data?.token),
+        fetcherGet("/Matris/imza-atma-gunluk-matris", session.data?.token),
       ]);
       setYaratma(rolAtamalar);
       setImzaAtma(imzaAtma);
       setImzaAtanan(imzaAtanan);
-      // console.log(imzaAtanan);
+
+      const formattedGunlukTalepYaratma = formatDataList(gunlukTalepYaratma);
+      setGunlukTalepYaratma(formattedGunlukTalepYaratma);
+
+      const formattedGunlukImzaAtma = formatDataList(gunlukImzaAtma);
+      setGunlukImzaAtma(formattedGunlukImzaAtma);
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
@@ -40,10 +52,16 @@ export default function RaporPage() {
             <TabsTrigger className='text-xl' value="talep-yaratma" disabled={isPending}>Talep Yaratma</TabsTrigger>
             <TabsTrigger className='text-xl' value="imza-atma" disabled={isPending}>Imza Atma</TabsTrigger>
             <TabsTrigger className='text-xl' value="imza-atanan" disabled={isPending}>Imza Atanan</TabsTrigger>
+            <TabsTrigger className='text-xl' value="gunluk" disabled={isPending}>Gunluk</TabsTrigger>
           </TabsList>
           <TabsContent value="talep-yaratma"><TalepYaratmaGrid data={yaratma} /></TabsContent>
           <TabsContent value="imza-atma"><ImzaAtmaGrid data={imzaAtma} /></TabsContent>
           <TabsContent value="imza-atanan"><ImzaAtananGrid data={imzaAtanan} /></TabsContent>
+          <TabsContent value="gunluk" className='flex gap-4'>
+            <AreaChartComp data={gunlukTalepYaratma} chartLabel={'Gunluk Talep Yaratma'} quantityValue={'talepSayisi'} quantityLabel={'Talep Sayisi'} />
+            <AreaChartComp data={gunlukImzaAtma} chartLabel={'Gunluk Imza Atma'} quantityValue={'imzaSayisi'} quantityLabel={'Imza Sayisi'} />
+          </TabsContent>
+
         </Tabs>
 
       </div>
