cb15e6ef92998cc849921301298e28f77e768daa
BurakPozut-CredeTech <burak.pozut@crede-tech.com>
2024-11-15 16:13:04 +0300

    Refactor role retrieval to use database queries and update type definitions

--- actions/kisi-rol.ts
+++ actions/kisi-rol.ts
@@ -5,8 +5,6 @@ import { db } from "@/lib/db";
 import { fetcherGet } from "@/utils";
 
 export async function kisininRolleri(kisiName: string): Promise<string[]> {
-  const session = await auth();
-
   //Get the kisi surname from the name by splinting the spaces
   const kisiNameArray = kisiName.split(" ");
   const kisiSurname = kisiNameArray[kisiNameArray.length - 1];
@@ -16,20 +14,27 @@ export async function kisininRolleri(kisiName: string): Promise<string[]> {
     },
   });
 
-  const roller = await fetcherGet(
-    `/Kisi/roller/${kisi?.KisiId}`,
-    session?.token
-  );
+  const roller = await db.kisiRol
+    .findMany({
+      where: {
+        KisiId: kisi?.KisiId,
+      },
+      select: {
+        Rol: {
+          select: {
+            RolAdi: true,
+          },
+        },
+      },
+    })
+    .then((data) => data.map((rol) => rol.Rol.RolAdi));
 
-  const rol_adları = roller.map((rol: { rolAdi: string }) => rol.rolAdi);
-  return rol_adları;
+  return roller;
 }
 
 export async function kisiAtanabilirRoller(
   kisiName: string
 ): Promise<string[]> {
-  const session = await auth();
-
   //Get the kisi surname from the name by splinting the spaces
   const kisiNameArray = kisiName.split(" ");
   const kisiSurname = kisiNameArray[kisiNameArray.length - 1];
@@ -39,25 +44,31 @@ export async function kisiAtanabilirRoller(
     },
   });
 
-  const kisiRoller = await fetcherGet(
-    `/Kisi/roller/${kisi?.KisiId}`,
-    session?.token
-  );
+  const kisiRoller = await db.kisiRol
+    .findMany({
+      where: {
+        KisiId: kisi?.KisiId,
+      },
+      select: {
+        Rol: {
+          select: {
+            RolAdi: true,
+          },
+        },
+      },
+    })
+    .then((data) => data.map((rol) => rol.Rol.RolAdi));
 
-  const roller = await fetcherGet(`/Rol`, session?.token);
+  const roller = await db.rol
+    .findMany({
+      select: {
+        RolAdi: true,
+      },
+    })
+    .then((data) => data.map((rol) => rol.RolAdi));
 
-  // Filter the roller array to remove roles that are already assigned to the kisi (present in kisiRoller)
-  const filteredRoller = roller.filter(
-    (rol: { rolAdi: string }) =>
-      !kisiRoller.some(
-        (kisiRol: { rolAdi: string }) => kisiRol.rolAdi === rol.rolAdi
-      )
-  );
-
-  const filteredRollerNames = filteredRoller.map(
-    (rol: { rolAdi: string }) => rol.rolAdi
-  );
-  return filteredRollerNames;
+  const availableRoles = roller.filter((role) => !kisiRoller.includes(role));
+  return availableRoles; // These are the roles not assigned to the Kisi
 }
 
 export async function kisininYetkileri(kisiName: string): Promise<[]> {
