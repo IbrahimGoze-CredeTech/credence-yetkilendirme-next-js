d83fbb437b02fc0c02d96dc6c468bb4862200652
can-dilber <can-dilber@hotmail.com>
2024-12-05 16:46:01 +0300

    raporlar düzenlemesi

--- app/rapor/matris-client.tsx
+++ app/rapor/matris-client.tsx
@@ -1,32 +1,32 @@
 // /* eslint-disable @typescript-eslint/no-explicit-any */
 
-'use client'
+"use client";
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
-import React, { useEffect, useState } from 'react'
-import { PieChartComp } from './_components/pie-chart'
-import { AreaChartComp } from './_components/area-chart'
-import RiskGrid from './_components/risk-grid'
-import ImzaAtananGrid from './_components/imza-atanan-grid'
-import ImzaOraniGrid from './_components/imza-orani-grid'
-import RolDagilimiPieChart from './_components/rol-dagilimi-piechart'
-import TalepYaratmaGrid from './_components/talep-yaratma-grid'
-import ImzaAtmaGrid from './_components/imza-atma-grid'
-import { MultipleBarChartData } from './_components/bar-chart-multiple'
-import { IImzaOraniMatris, ImzaAtmaKisi } from '@/types'
-import VerimlilikGrid from './_components/verimlilik-grid'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import React, { useEffect, useState } from "react";
+import { PieChartComp } from "./_components/pie-chart";
+import { AreaChartComp } from "./_components/area-chart";
+import RiskGrid from "./_components/risk-grid";
+import ImzaAtananGrid from "./_components/imza-atanan-grid";
+import ImzaOraniGrid from "./_components/imza-orani-grid";
+import RolDagilimiPieChart from "./_components/rol-dagilimi-piechart";
+import TalepYaratmaGrid from "./_components/talep-yaratma-grid";
+import ImzaAtmaGrid from "./_components/imza-atma-grid";
+import { MultipleBarChartData } from "./_components/bar-chart-multiple";
+import { IImzaOraniMatris, ImzaAtmaKisi } from "@/types";
+import VerimlilikGrid from "./_components/verimlilik-grid";
 
 interface Props {
-  talepYaratma: [],
-  imzaAtma: [],
-  imzaAtanan: [],
-  talepYaratmaGunluk: [],
-  imzaAtmaGunluk: [],
-  talepTipi: [],
-  kisiRisk: [],
-  rolDagilimi: [],
-  imzaOrani: IImzaOraniMatris[],
-  kisiVerimlilik: []
+  talepYaratma: [];
+  imzaAtma: [];
+  imzaAtanan: [];
+  talepYaratmaGunluk: [];
+  imzaAtmaGunluk: [];
+  talepTipi: [];
+  kisiRisk: [];
+  rolDagilimi: [];
+  imzaOrani: IImzaOraniMatris[];
+  kisiVerimlilik: [];
 }
 
 export default function MatrisClient({
@@ -39,9 +39,11 @@ export default function MatrisClient({
   kisiRisk,
   rolDagilimi,
   imzaOrani,
-  kisiVerimlilik
+  kisiVerimlilik,
 }: Props) {
-  const [combinedArray, setCombinedArray] = useState<MultipleBarChartData[]>([]);
+  const [combinedArray, setCombinedArray] = useState<MultipleBarChartData[]>(
+    []
+  );
 
   useEffect(() => {
     // Imza atma ve atanma kombinasyonu
@@ -59,17 +61,38 @@ export default function MatrisClient({
   }, [imzaAtanan, imzaAtma]);
 
   return (
-    <Tabs defaultValue="ozet" className="w-[95vw] flex flex-col items-center justify-center p-4">
+    <Tabs
+      defaultValue="gunluk"
+      className="w-[95vw] flex flex-col items-center justify-center p-4"
+    >
       <TabsList className="bg-gray-200 p-2 py-6">
-        <TabsTrigger value="talep-yaratma" className="text-xl">Talep Yaratma</TabsTrigger>
-        <TabsTrigger value="imza-atma" className="text-xl">İmza Atma</TabsTrigger>
-        <TabsTrigger value="imza-atanan" className="text-xl">İmza Atanan</TabsTrigger>
-        <TabsTrigger value="imza-atma-oranı" className="text-xl">İmza Oranı</TabsTrigger>
-        <TabsTrigger value="risk" className="text-xl">Risk</TabsTrigger>
-        <TabsTrigger value="gunluk" className="text-xl">Günlük</TabsTrigger>
-        <TabsTrigger value="talep-tipleri" className="text-xl">Talep Tipleri</TabsTrigger>
-        <TabsTrigger value="rol-dagilimi" className="text-xl">Rol Dağılımı</TabsTrigger>
-        <TabsTrigger value="kisi-verimlilik" className="text-xl">Kişi Verimlilik</TabsTrigger>
+        <TabsTrigger value="talep-yaratma" className="text-xl">
+          Talep Yaratma
+        </TabsTrigger>
+        <TabsTrigger value="imza-atma" className="text-xl">
+          İmza Atma
+        </TabsTrigger>
+        <TabsTrigger value="imza-atanan" className="text-xl">
+          İmza Atanan
+        </TabsTrigger>
+        <TabsTrigger value="imza-atma-oranı" className="text-xl">
+          İmza Oranı
+        </TabsTrigger>
+        <TabsTrigger value="risk" className="text-xl">
+          Risk
+        </TabsTrigger>
+        <TabsTrigger value="gunluk" className="text-xl">
+          Günlük
+        </TabsTrigger>
+        <TabsTrigger value="talep-tipleri" className="text-xl">
+          Talep Tipleri
+        </TabsTrigger>
+        <TabsTrigger value="rol-dagilimi" className="text-xl">
+          Rol Dağılımı
+        </TabsTrigger>
+        <TabsTrigger value="kisi-verimlilik" className="text-xl">
+          Kişi Verimlilik
+        </TabsTrigger>
       </TabsList>
 
       <TabsContent value="talep-yaratma">
@@ -111,5 +134,5 @@ export default function MatrisClient({
         <VerimlilikGrid data={kisiVerimlilik} />
       </TabsContent>
     </Tabs>
-  )
+  );
 }
